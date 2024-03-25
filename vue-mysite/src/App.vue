dce40e414c9db0acdcda006c007916a1bbe0b49e
koyoung99 <oayoung99@naver.com>
2024-03-25 17:56:04 +0900

    vue-mysite

---
+++ vue-mysite/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+//메인페이지 또는 공통 css
+import "@/assets/css/mysite.css";
+import "@/assets/css/main.css";
+export default {
+
+};
+
+</script>
+<style></style>
